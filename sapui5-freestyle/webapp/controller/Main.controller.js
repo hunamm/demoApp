4bc6915d80b8789a47839fd235b2c750d4034c44
Manuel Nonnenmacher <manuel@nonnenmacher-consulting.com>
2023-08-29 16:48:43 +0200

    Eigene Object Page

--- sapui5-freestyle/webapp/controller/Main.controller.js
+++ sapui5-freestyle/webapp/controller/Main.controller.js
@@ -1,9 +1,15 @@
-sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController, MessageBox) {
+sap.ui.define(["./BaseController", "sap/m/MessageBox", "sap/m/library"], function (BaseController, MessageBox, mobileLibrary) {
 	"use strict";
+
+	var URLHelper = mobileLibrary.URLHelper;
 
 	return BaseController.extend("com.nm.myapp.controller.Main", {
 		sayHello: function () {
 			MessageBox.show("Hello World!");
+		},
+
+		onContact: function () {
+			URLHelper.triggerEmail("manuel@nonnenmacher-conslting.com", "Info Request", false, false, false, true);
 		}
 	});
 });
